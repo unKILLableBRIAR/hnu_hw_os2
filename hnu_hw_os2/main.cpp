d50f076311377437d74534fddc6058ad03430bde
unKILLableBRIAR <wnsgud3590@naver.com>
2024-06-03 00:01:10 +0900

    stack 자료구조 구현

--- hnu_hw_os2/main.cpp
+++ hnu_hw_os2/main.cpp
@@ -1,24 +1,55 @@
 #include <iostream>
-#include <thread>
-#include <mutex>
-#include <windows.h>
 
 using namespace std;
-mutex mtx;
 
-void worker(int id, int delay_ms) {
-	for (int i = 0; i < 10; i++) {
-		Sleep(delay_ms);
-		mtx.lock();
-		cout << "thread#" << id << ": " << i << '\n';
-		mtx.unlock();
+typedef struct Node {
+	int data;
+	struct Node* next;
+}Node;
+
+int isEmpty(Node* top) {
+	if (top == NULL) return 1;
+	else return 0;
+}
+
+void push(Node** top, int data) {
+	Node* tmp = new Node;
+	tmp->data = data;
+	tmp->next = *top;
+	*top = tmp;
+}
+
+int pop(Node** top) {
+	int tmp_int;
+	Node* tmp = *top;
+
+	if (*top == NULL) {
+		cout << "Stack is empty.\n";
+		return 0;
+	}
+	else {
+		tmp_int = tmp->data;
+		*top = tmp->next;
+		delete(tmp);
+		return tmp_int;
 	}
 }
 
 int main() {
-	thread t1(worker, 1, 1000);
-	thread t2(worker, 2, 300);
-	t1.join();
-	t2.join();
-	printf("===end of main()===\n");
+	Node* top = NULL;
+	for (int i = 0; i < 10; i++) {
+		push(&top, i * 2 + 17);
+	}
+
+	cout << isEmpty(top) << '\n';
+
+	for (int i = 0; i < 10; i++) {
+		cout << pop(&top) << '\n';
+	}
+
+	cout << isEmpty(top) << '\n';
+
+	delete(top);
+
+	return 0;
 }
